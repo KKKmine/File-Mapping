6f3796bd4dceb37b2e15b681d2b2b074e21b2b95
KKKmine <tt40616@gmail.com>
2018-03-29 21:52:42 +0800

    Modify : Add input bool isBlocking in function recvMsg and sendMsg

--- FileMapping/FileMapping.h
+++ FileMapping/FileMapping.h
@@ -63,29 +63,33 @@ public:
 		return pBuf && hMapFile;
 	}
 
-	// Block process until send success
 	// INPUT msg : send data ptr
 	// INPUT len : msg size (Bytes)
-	void SendMsg(const void* msg, size_t len) const {
+	// INPUT isBlocking : Block process until send success when it's true
+	void SendMsg(const void* msg, size_t len, bool isBlocking) const {
 		if (hMapFile == NULL) throw ER_CREATE_MAPPING_FAIL;
 		if (pBuf == NULL) throw ER_ACCESS_MAPPING_FAIL;
 		if (len > BUF_SIZE) throw ER_SIZE_OUT_OF_RANGE;
 		*(BYTE*)pBuf = len;
 		CopyMemory((BYTE*)pBuf + 1, msg, len);
-		while (*(BYTE*)pBuf) { // Block process until msg size = 0
-			Sleep(10);
+		if (isBlocking) {
+			while (*(BYTE*)pBuf) { // Block process until msg size = 0
+				Sleep(10);
+			}
 		}
 	}
 
-	// Block process until recv success
 	// INPUT msg : recv container ptr
 	// INPUT len : msg max size (Bytes)
+	// INPUT isBlocking : Block process until recv success when it's true
 	// OUTPUT return : recv size (Bytes)
-	int RecvMsg(void* msg, size_t len) {
+	int RecvMsg(void* msg, size_t len, bool isBlocking) {
 		if (hMapFile == NULL) throw ER_CREATE_MAPPING_FAIL;
 		if (pBuf == NULL) throw ER_ACCESS_MAPPING_FAIL;
-		while (!*(BYTE*)pBuf) {  // Block process until msg size > 0
-			Sleep(10);
+		if (isBlocking) {
+			while (!*(BYTE*)pBuf) {  // Block process until msg size > 0
+				Sleep(10);
+			}
 		}
 		if (*(BYTE*)pBuf > len) throw ER_SIZE_NOT_ENOUGH;
 		CopyMemory(msg, (BYTE*)pBuf + 1, *(BYTE*)pBuf);
--- FileMapping/Source.cpp
+++ FileMapping/Source.cpp
@@ -12,19 +12,19 @@ int main() {
 		// Sender
 		if (mode == 0) {
 			char msg[] = "_ YOOOOOOOOOOOOOOOO Hello";
-			fm.SendMsg(msg, (strlen(msg) + 1) * sizeof(char));
+			fm.SendMsg(msg, (strlen(msg) + 1) * sizeof(char), true);
 
 			int num[] = { 0, 20, 30, 5 };
-			fm.SendMsg(num, 4 * sizeof(int));
+			fm.SendMsg(num, 4 * sizeof(int), true);
 		}
 		// Recvier
 		else {
 			char msg[50];
-			fm.RecvMsg(msg, 50 * sizeof(char));
+			fm.RecvMsg(msg, 50 * sizeof(char), true);
 			cout << "Get Message : " << msg << "\n";
 
 			int num[10];
-			int getSize = fm.RecvMsg(num, 10 * sizeof(int));
+			int getSize = fm.RecvMsg(num, 10 * sizeof(int), true);
 			cout << "Get Message : ";
 			for (int i = 0; i < getSize / sizeof(int); i++) {
 				cout << num[i] << " ";
